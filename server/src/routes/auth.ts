7d8b0077b6ea52b1e02fd24f0f0fb314cdc19ae8
nevz <projectnevz@gmail.com>
2022-08-29 22:29:22 +0800

    Add signUp route

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,9 +1,11 @@
 import express from "express";
 
-import { signIn } from "../controllers/authController.";
+import { signIn, signUp } from "../controllers/authController.";
 
 const router = express.Router();
 
 router.route("/api/signIn").post(signIn);
+
+router.route("/api/signUp").post(signUp);
 
 export default router;
