f8e9b3b82fcc18c02a31778c35e4722eefae3731
nevz <projectnevz@gmail.com>
2022-08-30 00:31:44 +0800

    Add validators to routes

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,11 +1,13 @@
 import express from "express";
 
 import { signIn, signUp } from "../controllers/authController.";
+import isValid from "../middlewares/isValid";
+import { validateSignIn, validateSignUp } from "../utils/validators";
 
 const router = express.Router();
 
-router.route("/api/signIn").post(signIn);
+router.route("/api/signIn").post(validateSignIn(), isValid, signIn);
 
-router.route("/api/signUp").post(signUp);
+router.route("/api/signUp").post(validateSignUp(), isValid, signUp);
 
 export default router;
