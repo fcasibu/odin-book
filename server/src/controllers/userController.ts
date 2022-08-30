e7624ebe8b3d4d8b339592497367e0426ebdcb25
nevz <projectnevz@gmail.com>
2022-08-30 22:08:12 +0800

    Add updateUser

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -7,3 +7,17 @@ export const getUser = catchAsync(async (req, res, next) => {
   const user = await User.findById(req.params.userID, "-password").exec();
   return sendResponse(res, 200, { user });
 });
+
+export const updateUser = catchAsync(async (req, res, next) => {
+  const { firstName, lastName, bio, password } = req.body;
+
+  const user = await User.findOne({ _id: req.params.userID }).exec();
+
+  if (user) {
+    Object.assign(user, { firstName, lastName, bio, password });
+    await user.save();
+    user.password = "";
+  }
+
+  return sendResponse(res, 200, { user });
+});
