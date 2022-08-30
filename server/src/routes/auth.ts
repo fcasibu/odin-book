33d48fd10365a18b4dd5d80a1d22197da3ff54e1
nevz <projectnevz@gmail.com>
2022-08-30 12:31:07 +0800

    Add verify route

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,6 +1,6 @@
-import express from "express";
+import express, { Request, Response } from "express";
 
-import { signIn, signUp } from "../controllers/authController.";
+import { signIn, signUp, verify } from "../controllers/authController.";
 import isValid from "../middlewares/isValid";
 import { validateSignIn, validateSignUp } from "../utils/validators";
 
@@ -9,5 +9,12 @@ const router = express.Router();
 router.route("/api/signIn").post(validateSignIn(), isValid, signIn);
 
 router.route("/api/signUp").post(validateSignUp(), isValid, signUp);
+
+router.route("/api/verify").get(verify, (req: Request, res: Response) => {
+  return res.status(200).json({
+    status: "success",
+    user: req.user,
+  });
+});
 
 export default router;
