de73d1a1d11e8e6feb45ea2ba882bf86fe2b50bb
nevz <projectnevz@gmail.com>
2022-08-30 22:09:28 +0800

    Add PATCH request for /:userID

--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -1,12 +1,12 @@
 import express from "express";
 
 import { verify } from "../controllers/authController.";
-import { getUser } from "../controllers/userController";
+import { getUser, updateUser } from "../controllers/userController";
 
 const router = express.Router();
 
 router.use(verify);
 
-router.route("/:userID").get(getUser);
+router.route("/:userID").get(getUser).patch(updateUser);
 
 export default router;
