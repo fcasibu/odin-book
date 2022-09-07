9a514f93276a7097449a3116f5890f19cfab0949
nevz <projectnevz@gmail.com>
2022-09-07 19:15:13 +0800

    Add verify middleware

--- server/src/routes/auction.ts
+++ server/src/routes/auction.ts
@@ -1,8 +1,11 @@
 import express from 'express';
 
 import { getAllAuctions } from '../controllers/auctionController';
+import { verify } from '../controllers/authController';
 
 const router = express.Router();
+
+router.use(verify);
 
 router.route('/').get(getAllAuctions);
 
