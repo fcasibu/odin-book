7429dea63f339377dfb0c86ab47d202318f62596
nevz <projectnevz@gmail.com>
2022-09-07 20:03:36 +0800

    Add createAuction

--- server/src/controllers/auctionController.ts
+++ server/src/controllers/auctionController.ts
@@ -1,4 +1,5 @@
-import { Auction } from "../model/auction";
+import { Auction, AuctionItem } from "../model/auction";
+import { IUser } from "../model/user";
 
 import catchAsync from "../utils/catchAsync";
 import sendResponse from "../utils/sendResponse";
@@ -8,4 +9,18 @@ export const getAllAuctions = catchAsync(async (req, res, next) => {
     const auctions = await Auction.find({ active: { $ne: false } }).exec();
 
     return sendResponse(res, 200, { auctions });
+})
+
+export const createAuction = catchAsync(async (req, res, next) => {
+    const { id } = req.user as IUser;
+    const auction = await Auction.create({
+        ...req.body,
+        seller: id,
+    });
+    const item = await AuctionItem.create({
+        ...req.body,
+        auction
+    });
+
+    return sendResponse(res, 201, { item });
 })
--- server/src/routes/auction.ts
+++ server/src/routes/auction.ts
@@ -1,12 +1,12 @@
 import express from 'express';
 
-import { getAllAuctions } from '../controllers/auctionController';
+import { createAuction, getAllAuctions } from '../controllers/auctionController';
 import { verify } from '../controllers/authController';
 
 const router = express.Router();
 
 router.use(verify);
 
-router.route('/').get(getAllAuctions);
+router.route('/').get(getAllAuctions).post(createAuction);
 
 export default router;
