e41bc95bc5b8383ce51c1e585474d41f5a7ffec5
nevz <projectnevz@gmail.com>
2022-09-09 14:49:33 +0800

    Add GET for specific auction and populated find queries

--- server/src/controllers/auctionController.ts
+++ server/src/controllers/auctionController.ts
@@ -6,19 +6,32 @@ import sendResponse from "../utils/sendResponse";
 
 
 export const getAllAuctions = catchAsync(async (req, res, next) => {
-    const auctions = await Auction.find({ active: { $ne: false } }).exec();
+    const auctions = await Auction.find({ active: { $ne: false } })
+        .populate({ path: 'item' }).exec();
 
     return sendResponse(res, 200, { auctions });
+})
+
+export const getAuction = catchAsync(async (req, res, next) => {
+    const auction = await Auction.findById(req.params.auctionID)
+        .populate({ path: 'item' }).exec();
+
+    return sendResponse(res, 200, { auction });
 })
 
 export const createAuction = catchAsync(async (req, res, next) => {
     const { id } = req.user as IUser;
     const auction = await Auction.create({
-        ...req.body,
+        category: req.body.category,
+        description: req.body.description,
+        startingBid: req.body.startingBid,
+        startDate: req.body.startDate,
+        endDate: req.body.endDate,
         seller: id,
     });
     const item = await AuctionItem.create({
-        ...req.body,
+        title: req.body.title,
+        photoURL: req.body.photoURL, // change to multer/cloudinary later
         auction
     });
 
