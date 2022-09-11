c70bd9c80f276e471a74c9a06795e4db15f846fe
nevz <projectnevz@gmail.com>
2022-09-11 20:13:12 +0800

    Add test for PATCH and DELETE

--- server/src/__tests__/auction.spec.ts
+++ server/src/__tests__/auction.spec.ts
@@ -1,6 +1,7 @@
 import request from "supertest";
 
 import app from "../app";
+import { Auction, AuctionItem } from "../model/auction";
 import Category from "../model/category";
 import User from "../model/user";
 import { connect, close, clear } from "../mongoConfigTesting";
@@ -15,12 +16,12 @@ const user = {
 };
 
 const auction = {
-    title: 'Test Item',
-    description: 'Test description',
+    title: "Test Item",
+    description: "Test description",
     startingBid: 500,
-    photoURL: '',
+    photoURL: "",
     startDate: Date.now(),
-    endDate: new Date('2050').getTime()
+    endDate: new Date("2050").getTime(),
 };
 
 let token: string;
@@ -30,7 +31,21 @@ let categoryID: string;
 beforeAll(async () => {
     await connect();
     const testUser = await User.create(user);
-    const testCategory = await Category.create({ title: 'test_category' });
+    const testCategory = await Category.create({ title: "test_category" });
+    const testAuction = await Auction.create({
+        category: testCategory._id.toString(),
+        description: auction.description,
+        startingBid: auction.startingBid,
+        startDate: auction.startDate,
+        endDate: auction.endDate,
+        seller: testUser._id.toString(),
+    });
+    await AuctionItem.create({
+        title: auction.title,
+        photoURL: auction.photoURL,
+        auction: testAuction._id.toString(),
+    });
+    auctionID = testAuction._id.toString();
     categoryID = testCategory._id.toString();
     token = signToken(testUser._id);
 });
@@ -41,50 +56,52 @@ afterAll(async () => {
     jest.clearAllMocks();
 });
 
-describe("POST /api/auctions", () => {
-    it("should create a new auction", done => {
-        request(app)
-            .post('/api/auctions')
-            .type('form')
-            .send({ ...auction, category: categoryID })
-            .set("Authorization", `Bearer ${token}`)
-            .expect('Content-Type', /json/)
-            .expect(201)
-            .end((err, res) => {
-                if (err) return done(err);
-                request(app)
-                expect(res.body.status).toMatch(/success/i);
-                auctionID = res.body.item.auction._id.toString();
-                expect(res.body.item).toBeTruthy();
-                expect(res.body.item.title).toBe(auction.title);
-                expect(res.body.item.auction.description).toBe(auction.description);
-                expect(res.body.item.auction.startingBid).toBe(auction.startingBid);
-                expect(res.body.item.auction.startDate).toBe(new Date(auction.startDate).toISOString());
-                expect(res.body.item.auction.endDate).toBe(new Date(auction.endDate).toISOString());
-                return done();
-            })
-    })
-})
-
 describe("GET /api/auctions/", () => {
-    it("should retrieve all the active auctions", done => {
+    it("should retrieve all the active auctions", (done) => {
         request(app)
-            .get('/api/auctions/')
+            .get("/api/auctions/")
             .set("Authorization", `Bearer ${token}`)
             .expect("Content-Type", /json/)
             .expect(200)
             .end((err, res) => {
                 if (err) return done(err);
-                expect(res.body.status).toMatch(/success/i)
+                expect(res.body.status).toMatch(/success/i);
                 expect(res.body.auctions).toBeTruthy();
                 expect(res.body.auctions).toHaveLength(1);
                 return done();
-            })
-    })
-})
+            });
+    });
+});
 
-describe("GET /api/auctions/auctionID", () => {
-    it("should retrieve all the active auctions", done => {
+describe("POST /api/auctions", () => {
+    it("should create a new auction", (done) => {
+        request(app)
+            .post("/api/auctions")
+            .type("form")
+            .send({ ...auction, category: categoryID })
+            .set("Authorization", `Bearer ${token}`)
+            .expect("Content-Type", /json/)
+            .expect(201)
+            .end((err, res) => {
+                if (err) return done(err);
+                request(app);
+                expect(res.body.status).toMatch(/success/i);
+                expect(res.body.item).toBeTruthy();
+                expect(res.body.item.title).toBe(auction.title);
+                expect(res.body.item.auction.description).toBe(auction.description);
+                expect(res.body.item.auction.startingBid).toBe(auction.startingBid);
+                expect(res.body.item.auction.startDate).toBe(
+                    new Date(auction.startDate).toISOString()
+                );
+                expect(res.body.item.auction.endDate).toBe(new Date(auction.endDate).toISOString());
+                expect(res.body.item.auction.active).toBeTruthy();
+                return done();
+            });
+    });
+});
+
+describe("GET /api/auctions/:auctionID", () => {
+    it("should retrieve all the active auctions", (done) => {
         request(app)
             .get(`/api/auctions/${auctionID}`)
             .set("Authorization", `Bearer ${token}`)
@@ -92,12 +109,51 @@ describe("GET /api/auctions/auctionID", () => {
             .expect(200)
             .end((err, res) => {
                 if (err) return done(err);
-                expect(res.body.status).toMatch(/success/i)
+                expect(res.body.status).toMatch(/success/i);
                 expect(res.body.auction).toBeTruthy();
-                expect(res.body.auction.item).toBeTruthy();
+                expect(res.body.auction.active).toBeTruthy();
                 expect(res.body.auction.description).toBe(auction.description);
+                expect(res.body.auction.item).toBeTruthy();
                 expect(res.body.auction.item.title).toBe(auction.title);
                 return done();
-            })
-    })
-})
+            });
+    });
+});
+
+describe("PATCH /api/auctions/:auctionID", () => {
+    it("should update the auction", (done) => {
+        request(app)
+            .patch(`/api/auctions/${auctionID}`)
+            .type("form")
+            .send({ title: "New Title", description: "New Description" })
+            .set("Authorization", `Bearer ${token}`)
+            .expect("Content-Type", /json/)
+            .expect(200)
+            .end((err, res) => {
+                if (err) return done(err);
+                expect(res.body.status).toMatch(/success/i);
+                expect(res.body.item).toBeTruthy();
+                expect(res.body.item.title).toMatch(/new title/i);
+                expect(res.body.item.auction.description).toMatch(/new description/i);
+                return done();
+            });
+    });
+});
+
+describe("DELETE /api/auctions/:auctionID", () => {
+    it("should set the auction's active field to false and respond with a success message", (done) => {
+        request(app)
+            .delete(`/api/auctions/${auctionID}`)
+            .set("Authorization", `Bearer ${token}`)
+            .expect("Content-Type", /json/)
+            .expect(200)
+            .end(async (err, res) => {
+                if (err) return done(err);
+                expect(res.body.status).toMatch(/success/i);
+                expect(res.body.message).toMatch(/successfully deleted the auction/i);
+                const auction = await Auction.findById(auctionID);
+                expect(auction?.active).toBeFalsy();
+                return done();
+            });
+    });
+});
