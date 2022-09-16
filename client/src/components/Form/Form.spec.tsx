9d997209f38cdade60acfd3a1534fd79cd95631f
nevz <projectnevz@gmail.com>
2022-09-16 21:36:47 +0800

    Add test file for the Form Component

--- client/src/components/Form/Form.spec.tsx
+++ client/src/components/Form/Form.spec.tsx
@@ -1,2 +1,18 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import "@testing-library/jest-dom";
+import Form from "./Form";
+import { Input } from "./Input";
+import Button from "../Button";
+
+describe("Form", () => {
+    it("should render the form and match the snapshot", () => {
+        const { container } = render(
+            <Form>
+                <Input id="name" name="name" />
+                <Button type="submit">Submit</Button>
+            </Form>
+        );
+        expect(container).toMatchSnapshot();
+    });
+});
