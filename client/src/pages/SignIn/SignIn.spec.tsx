ae436d640e9d6162d11c8f0cccf493a29f7ce907
nevz <projectnevz@gmail.com>
2022-09-21 21:38:22 +0800

    Setup test file

--- client/src/pages/SignIn/SignIn.spec.tsx
+++ client/src/pages/SignIn/SignIn.spec.tsx
@@ -1,3 +1,19 @@
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
+import SignIn, { SignUp } from "./SignIn";
 
+describe("SignIn", () => {
+    it("should render and match the snapshot", () => {
+        const { container } = render(<SignIn />);
+
+        expect(container).toMatchSnapshot();
+    })
+})
+
+describe("SignUp", () => {
+    it("should render and match the snapshot", () => {
+        const { container } = render(<SignUp />);
+
+        expect(container).toMatchSnapshot();
+    })
+})
