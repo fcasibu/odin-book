bd281483951985966729b2a8d4dcb5053609a4e3
nevz <projectnevz@gmail.com>
2022-09-26 20:22:03 +0800

    Add Profile page

---
+++ client/src/pages/Profile/Profile.spec.tsx
@@ -0,0 +1,7 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+
+describe("Profile", () => {
+    it('should render the banner, profile picture, name, friends, and navigation', () => {
+    })
+})
