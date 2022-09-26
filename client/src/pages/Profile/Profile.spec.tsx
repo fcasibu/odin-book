bfc53705b99ac4a799b1eb2afa8edea0cc3d945e
nevz <projectnevz@gmail.com>
2022-09-26 20:32:17 +0800

    Add test file for Profile

--- client/src/pages/Profile/Profile.spec.tsx
+++ client/src/pages/Profile/Profile.spec.tsx
@@ -1,7 +1,14 @@
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
+import Profile from "./Profile";
 
 describe("Profile", () => {
-    it('should render the banner, profile picture, name, friends, and navigation', () => {
-    })
-})
+    it("should render the banner, profile picture, name, friends, and navigation", () => {
+        const mockUser = { firstName: "John", lastName: "Doe", friends: [{}], bannerURL: "", avatarURL: "" };
+        render(<Profile user={mockUser} />);
+        screen.getByAltText("Banner");
+        screen.getByAltText("Profile Picture");
+        screen.getByLabelText(mockUser.firstName);
+        screen.getByLabelText(`${mockUser.friends.length} friends`);
+    });
+});
